977d34460e247a2720234fd756d4624220f24771
rty0247 <t.tarun.2001@gmail.com>
2024-05-30 16:32:47 +0530

    DB Config File Changed

--- src/config/db.js
+++ src/config/db.js
@@ -1,19 +1,19 @@
-// // src/config/db.js
+// src/config/db.js
 
-// const { Sequelize } = require('sequelize');
+const { Sequelize } = require('sequelize');
 
-// const sequelize = new Sequelize('storm', 'MySqlUser', 'Admin@1234', {
-//   host: '49.207.11.223',
-//   dialect: 'mysql'
-// });
+const sequelize = new Sequelize('storm', 'MySqlUser', 'Admin@1234', {
+  host: '49.207.11.223',
+  dialect: 'mysql'
+});
 
-// (async () => {
-//   try {
-//     await sequelize.authenticate();
-//     console.log('Connection has been established successfully.');
-//   } catch (error) {
-//     console.error('Unable to connect to the database:', error);
-//   }
-// })();
+(async () => {
+  try {
+    await sequelize.authenticate();
+    console.log('Connection has been established successfully.');
+  } catch (error) {
+    console.error('Unable to connect to the database:', error);
+  }
+})();
 
-// module.exports = sequelize;
+module.exports = sequelize;
